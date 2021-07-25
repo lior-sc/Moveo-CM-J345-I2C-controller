37b8ff9113e313fc242e90f1a09e65b43ed488cd
Lior S <lior@soltrex.io>
2021-07-25 20:00:43 +0300

    I2c still not working

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include "TrapezStepper.h"
 
-#define DEBUG			      true
+#define DEBUG			      	false
 
 // Pin definitions
 #define J3_EN_PIN		    	2
@@ -63,6 +63,17 @@ double getSerialfloat(String str)
 	while (!Serial.available());
 	return (double) Serial.parseFloat();
 }
+void blink_led(int times, unsigned long int dt)
+{
+  for(int i=0;i<times;i++)
+  {
+    digitalWrite(13,HIGH);
+    delay(dt);
+    digitalWrite(13,LOW);
+    delay(dt);
+  }
+}
+
 // Stepper movement functions
 void moveSystem()
 {
@@ -133,7 +144,7 @@ void moveSystemJoints(double t3, double t4, double t5)
 void process_cmd(int size)
 {
 	// The buffers in the message received are arranged in Big Endian configuration (MSB first)
-
+	blink_led(3,50);
 	uint8_t buff[7];
 	uint8_t _cmd_type;
 	double _cmd_value[3];
